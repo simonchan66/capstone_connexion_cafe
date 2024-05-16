4366d678ecea59d0e245e8390075a158a6b87d91
simonchan66 <formul529@gmail.com>
2024-05-16 14:57:12 -0600

    still working on the crashes in orderpage

---
+++ app/Components/Navigation.js
@@ -0,0 +1,34 @@
+"use client";
+import Link from 'next/link';
+
+const Navigation = () => {
+
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">HOME</Link>
+        </li>
+        <li>
+          <Link href="/OrderPage">Order</Link>
+        </li>
+        <li>
+          <Link href="/AllStatsPage">Stats</Link>
+        </li>
+        <li>
+          <Link href="/EditProduct">Edit</Link>
+        </li>
+        <li>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            中文
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
---
+++ app/Components/OrderPage.js
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+
+const OrderPage = () => {
+  const [products, setProducts] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000); // Update the date every second
+
+    return () => clearInterval(interval); // Clean up the interval on component unmount
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const db = getFirestore();
+      const colRef = collection(db, 'products');
+
+      try {
+        const snapshot = await getDocs(colRef);
+        const productsData = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setProducts(productsData);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="order-page">
+      <header>
+        <h1>Connexion Cafe</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </header>
+
+      <div className="order-content">
+        <div className="coffee-items">
+          {products.map((product) => (
+            <div key={product.id} className="coffee-item">
+              <img src={product.image} alt={product.item_name} />
+              <p>{product.name}</p>
+              <p>${product.price.toFixed(2)}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderPage;
