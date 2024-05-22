644fae9896a53f95820bd1515ff8809eb338d322
David-Fok <foktatchun@gmail.com>
2024-05-22 08:33:32 -0600

    Components - EditProductPage.js added

---
+++ app/Components/EditProductPage.js
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const EditProductPage = () => {
+  return (
+    <div>
+      <h1>Edit Product Page</h1>
+    </div>
+  );
+};
