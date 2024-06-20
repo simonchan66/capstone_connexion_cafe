b2f1ac31f87aa5cb4c1edcb8d8981d99d4c3d47e
simonchan66 <formul529@gmail.com>
2024-06-20 15:01:05 -0600

    updated style again

--- app/Feedback/page.js
+++ app/Feedback/page.js
@@ -4,8 +4,8 @@ import RestaurantFeedbackForm from "../Components/RestaurantFeedbackForm";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">
+    <main className="bg-gray-800 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="bg-gray-800 text-3xl font-bold text-white mb-4">
         Customer Feedback
       </h1>
       <RestaurantFeedbackForm />
