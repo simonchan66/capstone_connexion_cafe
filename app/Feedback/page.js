7ba6bbc57c1b995700ae8252599d21b862fcee1f
suetsuey <140632841+suetsuey@users.noreply.github.com>
2024-06-23 21:42:19 -0600

    added responsive for feedback

--- app/Feedback/page.js
+++ app/Feedback/page.js
@@ -4,11 +4,15 @@ import RestaurantFeedbackForm from "../Components/RestaurantFeedbackForm";
 
 export default function Home() {
   return (
-    <main className="bg-gray-800 min-h-screen flex flex-col items-center justify-center">
-      <h1 className="bg-gray-800 text-3xl font-bold text-white mb-4">
-        Customer Feedback
-      </h1>
-      <RestaurantFeedbackForm />
+    <main className="bg-gray-800 min-h-screen flex flex-col items-center justify-center p-4 sm:p-8">
+      <div className="mb-8">
+        <h1 className="bg-gray-800 text-3xl font-bold text-white">
+          Customer Feedback
+        </h1>
+      </div>
+      <div className="w-full max-w-md">
+        <RestaurantFeedbackForm />
+      </div>
     </main>
   );
 }
