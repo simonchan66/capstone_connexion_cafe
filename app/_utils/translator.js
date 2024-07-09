bba9fd8bc4208dcf379f74a44b0027f43169240d
David-Fok <foktatchun@gmail.com>
2024-07-08 23:46:33 -0600

    try

--- app/_utils/translator.js
+++ app/_utils/translator.js
@@ -1,1 +1,11 @@
+import en from "./translations/en.json";
 
+const translations = {
+  en,
+};
+export default function translator(lang, key, defaultValue = "") {
+  const langTranslations = translations[lang];
+  return langTranslations && langTranslations[key]
+    ? langTranslations[key]
+    : defaultValue;
+}
