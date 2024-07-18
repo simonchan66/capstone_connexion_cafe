fbed12374fdc8ed36d15ca85db0f0aa422d059a8
David-Fok <foktatchun@gmail.com>
2024-07-18 13:07:52 -0600

    Feedback Dashboard en & zh

--- app/Components/FeedbackDashboard.js
+++ app/Components/FeedbackDashboard.js
@@ -17,6 +17,7 @@ import {
   Cell,
 } from "recharts";
 import ReactStars from "react-stars";
+import { useLanguage } from "../_utils/LanguageContext";
 
 // assisted by Copilot and Claude 3 Opus
 const FeedbackDashboard = () => {
@@ -66,6 +67,7 @@ const FeedbackDashboard = () => {
 
   const calculateCategoryPerformance = () => {
     const categories = ["Vibe", "Service", "Product", "Price", "Cleanliness"];
+
     const performanceData = categories.map((category) => {
       const totalScore = feedbackData.reduce(
         (sum, feedback) => sum + feedback[`${category.toLowerCase()}Rating`],
@@ -182,43 +184,38 @@ const FeedbackDashboard = () => {
     );
   };
 
+  const { t } = useLanguage();
+
   return (
     <div>
       <div className="items-center justify-between mb-4">
-
         <h1 className="text-xl font-bold">
-          Feedback Dashboard
+          {t("feedbackDashboard")}
           <button
             className={`text-sm mr-2 p-2 ml-6 ${
               timeFilter === "all" ? "bg-blue-500 text-white" : "bg-gray-600"
             }`}
             onClick={() => setTimeFilter("all")}
-
           >
-            All Time
+            {t("allTime")}
           </button>
           <button
-
             className={`text-sm mr-2 p-2 ${
               timeFilter === "today" ? "bg-blue-500 text-white" : "bg-gray-600"
             }`}
             onClick={() => setTimeFilter("today")}
-
           >
-            Today
+            {t("today")}
           </button>
           <button
-
-
             className={`text-sm p-2 ${
               timeFilter === "thisMonth"
                 ? "bg-blue-500 text-white"
                 : "bg-gray-600"
             }`}
             onClick={() => setTimeFilter("thisMonth")}
-
           >
-            This Month
+            {t("thisMonth")}
           </button>
         </h1>
 
@@ -228,7 +225,7 @@ const FeedbackDashboard = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
           {/* Overall Score */}
           <div className="bg-gray-700 p-4 rounded shadow">
-            <h2 className="text-lg font-bold mb-2">Overall Score</h2>
+            <h2 className="text-lg font-bold mb-2">{t("overallScore")}</h2>
             <div style={{ width: 120, height: 120, margin: "auto" }}>
               <CircularProgressbar
                 value={calculateOverallScore()}
@@ -244,12 +241,14 @@ const FeedbackDashboard = () => {
 
           {/* Category Performance */}
           <div className="bg-gray-700 p-4 rounded shadow">
-            <h2 className="text-lg font-bold mb-2">Category Performance</h2>
+            <h2 className="text-lg font-bold mb-2">
+              {t("categoryPerformance")}
+            </h2>
             <div className="space-y-2">
               {calculateCategoryPerformance().map((item) => (
                 <div key={item.category} className="flex items-center">
                   <span className="text-white font-semibold w-24">
-                    {item.category}:
+                    {t(item.category)}:
                   </span>
                   <ReactStars
                     count={5}
@@ -271,7 +270,7 @@ const FeedbackDashboard = () => {
           {/* Serving Time Distribution */}
           <div className="bg-gray-700 p-4 rounded shadow col-span-2 md:col-span-1 text-xs">
             <h2 className="text-lg font-bold mb-2 ">
-              Serving Time Distribution
+              {t("servingTimeDistribution")}
             </h2>
             <ResponsiveContainer width="100%" height={200}>
               <BarChart data={calculateServingTimeDistribution()}>
@@ -287,7 +286,7 @@ const FeedbackDashboard = () => {
 
           {/* Top Favorite Items */}
           <div className="bg-gray-700 p-4 rounded shadow">
-            <h2 className="text-lg font-bold mb-2">Top Favorite Items</h2>
+            <h2 className="text-lg font-bold mb-2">{t("topFavoriteItems")}</h2>
             <ol className="list-decimal list-inside">
               {getTopFavoriteItems(3).map((item) => (
                 <li key={item.item} className="text-sm">
@@ -300,7 +299,7 @@ const FeedbackDashboard = () => {
           {/* Recent Positive Feedbacks */}
           <div className="bg-gray-700 p-4 rounded shadow">
             <h2 className="text-lg font-bold mb-2">
-              Recent Positive Feedbacks
+              {t("recentPositiveFeedback")}
             </h2>
             <ul className="list-disc list-inside">
               {getRecentFeedbacks(3, "positive").map((feedback, index) => (
@@ -314,7 +313,7 @@ const FeedbackDashboard = () => {
           {/* Recent Negative Feedbacks */}
           <div className="bg-gray-700 p-4 rounded shadow">
             <h2 className="text-lg font-bold mb-2">
-              Recent Negative Feedbacks
+              {t("recentNegativeFeedback")}
             </h2>
             <ul className="list-disc list-inside">
               {getRecentFeedbacks(3, "negative").map((feedback, index) => (
@@ -327,7 +326,9 @@ const FeedbackDashboard = () => {
 
           {/* Browser Distribution */}
           <div className="bg-gray-700 p-4 rounded shadow col-span-2 md:col-span-1">
-            <h2 className="text-lg font-bold mb-2">Browser Distribution</h2>
+            <h2 className="text-lg font-bold mb-2">
+              {t("browserDistribution")}
+            </h2>
             <ResponsiveContainer width="100%" height={250}>
               <PieChart>
                 <Pie
@@ -354,7 +355,7 @@ const FeedbackDashboard = () => {
           </div>
           {/* Emoji Display */}
           <div className="bg-gray-700 p-4 rounded shadow col-span-2 md:col-span-1">
-            <h2 className="text-lg font-bold mb-2">Collected Emojis</h2>
+            <h2 className="text-lg font-bold mb-2">{t("collectedEmojis")}</h2>
             <div className="flex flex-wrap items-center justify-left">
               {feedbackData.map((feedback, index) => (
                 <span key={index} className="text-3xl pb-2">
